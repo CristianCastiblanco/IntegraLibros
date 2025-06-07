7f11289e13716c62e9425ef8a1a5c0ea884f4500
CristianCastiblanco <cmontes2589@gmail.com>
2025-06-07 17:08:57 -0500

    Configuration Jenkins

--- test/test.js
+++ test/test.js
@@ -3,6 +3,6 @@ import assert from 'node:assert'
 import { assert } from 'node:console'
 
 test ('fetch', async () => {
-    const response = await fetch('http://localhost:3000')
+    const response = await fetch('http://localhost:8081')
     assert.eqaul(response.status_code, 200)
 })
