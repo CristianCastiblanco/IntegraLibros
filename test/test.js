e8f352b083726ca6edce6f64bdbcac609904601f
CristianCastiblanco <cmontes2589@gmail.com>
2025-06-09 22:45:31 -0500

    Update image jenkins

--- test/test.js
+++ test/test.js
@@ -2,13 +2,15 @@ import test from 'node:test'
 import assert from 'node:assert'
 
 test ('fetch', async () => {
+  try {
     const response = await fetch('http://localhost:8081')
     
-    if (response.status != 200) {
+    if (response.status == 200) {
       // La respuesta fue exitosa
       console.log('Respuesta exitosa:', response.status)
-    } else {
-      // La respuesta no fue exitosa
-      console.error(`Error al obtener la página: ${response.status}`);
     }
+
+  } catch (error) {
+    console.log('Error durante la solicitud:', error);
+  }
 })
