cd472f8d2ef1cce0347b8b2c735e3006eda3d846
CristianCastiblanco <cmontes2589@gmail.com>
2025-06-23 22:30:06 -0500

    Add search in backend

---
+++ frontend/pages/home.js
@@ -0,0 +1,19 @@
+document.addEventListener("DOMContentLoaded", () => {
+    const btnSearch = document.getElementById("btnSearch");
+    const loginS = document.getElementById("loginS");
+
+    // Cambiar entre pestañas de Register/Login
+    btnSearch.addEventListener("click", async() => {
+        try{
+            const res = await fetch("http://localhost:3000/search")
+
+            if (res) {
+                alert(`${res}`);
+            }
+        }catch (e) {
+            console.error("Error al consultar la función search en el backend:", e);
+            alert("Hubo un problema al intentar listar los libros.");
+        }
+        
+    })
+})
